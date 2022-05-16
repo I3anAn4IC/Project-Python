0d2a80147b40d0d9d71647252b2dbaeefba33407
Student <andreartemi@gmail.com>
2022-05-16 09:33:05 +0400

    final

--- Project/project.py
+++ Project/project.py
@@ -3,12 +3,14 @@ import random
 import threading
 
 # иниициализация окна
+
 screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.init()
 FPS = 120
 clock = pg.time.Clock()
 
 # создание блоков
+
 W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 
@@ -22,10 +24,12 @@ class Button:
         self.active_color = active_color
 
     # "рисовка" кнопки на экране
+
     def draw_button(self, x, y, message=None, font=None, button_num=None, done=None):
-        global width_text, height_text
+        global width_text, height_text, score
         mouse = pg.mouse.get_pos()
         click = pg.mouse.get_pressed()
+
         if len(message) >= 1:
             font_button = pg.font.Font(None, self.height)
             text = font_button.render(message, True, self.height)
@@ -35,6 +39,88 @@ class Button:
         if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
             pg.draw.rect(self.screen, self.inactive_color, (x, y, self.width, self.height))
             if click[0] == 1:
+                if button_num == 3:
+                    if t[0] == min(t):
+                        score += 1
+                        t[0] = 10
+                    elif t[0] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 4:
+                    if t[1] == min(t):
+                        score += 1
+                        t[1] = 10
+                    elif t[1] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 5:
+                    if t[2] == min(t):
+                        score += 1
+                        t[2] = 10
+                    elif t[2] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 6:
+                    if t[3] == min(t):
+                        score += 1
+                        t[3] = 10
+                    elif t[3] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 7:
+                    if t[4] == min(t):
+                        score += 1
+                        t[4] = 10
+                    elif t[4] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 8:
+                    if t[5] == min(t):
+                        score += 1
+                        t[5] = 10
+                    elif t[5] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 9:
+                    if t[6] == min(t):
+                        score += 1
+                        t[6] = 10
+                    elif t[6] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 10:
+                    if t[7] == min(t):
+                        score += 1
+                        t[7] = 10
+                    elif t[7] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                if button_num == 11:
+                    if t[8] == min(t):
+                        score += 1
+                        t[8] = 10
+                    elif t[8] == 10:
+                        print('Error: 1')
+                    else:
+                        print('Error: 0')
+
+                print(score)
                 pg.time.delay(300)
         else:
             pg.draw.rect(self.screen, self.active_color, (x, y, self.width, self.height))
@@ -69,12 +155,14 @@ Block = Button(150, 100, screen, (247, 96, 159), (255, 0, 106))
 s = '123456789'
 t = []
 f = False
+score = 0
+win = False
 
 while s != '':
     h = random.choice(s)
     p = str(h)
     s = s.replace(p, '')
-    t.append(h)
+    t.append(int(h))
 
 
 coordinates = [[random.randint(75, (W // 3) - 150), random.randint(50, (H // 3) - 100)],
@@ -87,22 +175,25 @@ coordinates = [[random.randint(75, (W // 3) - 150), random.randint(50, (H // 3) 
                [random.randint(W//3, W-(W//3)-150), random.randint(H-(H//3), H-100)],
                [random.randint(W-(W//3), W-150), random.randint(H-(H//3), H-100)]]
 
+
 def funk():
+    global win
     screen.fill((0, 0, 0))
-    Block.draw_button(coordinates[0][0], coordinates[0][1], t[0], None, 3)
-    Block.draw_button(coordinates[1][0], coordinates[1][1], t[1], None, 4)
-    Block.draw_button(coordinates[2][0], coordinates[2][1], t[2], None, 5)
+    Block.draw_button(coordinates[0][0], coordinates[0][1], str(t[0]), None, 3)
+    Block.draw_button(coordinates[1][0], coordinates[1][1], str(t[1]), None, 4)
+    Block.draw_button(coordinates[2][0], coordinates[2][1], str(t[2]), None, 5)
 
-    Block.draw_button(coordinates[3][0], coordinates[3][1], t[3], None, 6)
-    Block.draw_button(coordinates[4][0], coordinates[4][1], t[4], None, 7)
-    Block.draw_button(coordinates[5][0], coordinates[5][1], t[5], None, 8)
+    Block.draw_button(coordinates[3][0], coordinates[3][1], str(t[3]), None, 6)
+    Block.draw_button(coordinates[4][0], coordinates[4][1], str(t[4]), None, 7)
+    Block.draw_button(coordinates[5][0], coordinates[5][1], str(t[5]), None, 8)
 
-    Block.draw_button(coordinates[6][0], coordinates[6][1], t[6], None, 9)
-    Block.draw_button(coordinates[7][0], coordinates[7][1], t[7], None, 10)
-    Block.draw_button(coordinates[8][0], coordinates[8][1], t[8], None, 11)
+    Block.draw_button(coordinates[6][0], coordinates[6][1], str(t[6]), None, 9)
+    Block.draw_button(coordinates[7][0], coordinates[7][1], str(t[7]), None, 10)
+    Block.draw_button(coordinates[8][0], coordinates[8][1], str(t[8]), None, 11)
 
     sec = False
     gameover = False
+
     while not gameover:
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
@@ -110,12 +201,18 @@ def funk():
                     screen.fill((0, 0, 0))
                     timer.cancel()
                     return
+
         while not sec:
-            timer = threading.Timer(2, draw_block)
+            timer = threading.Timer(10, draw_block)
             timer.start()
             sec = True
+
         if f:
             draw_block()
+
+        if score == 9 and not win:
+            print('You win')
+            win = True
 
         clock.tick(FPS)
         pg.display.flip()
@@ -124,5 +221,4 @@ def funk():
         pg.quit()
 
 
-# funk()
-
+funk()
--- Project/pygame_project.py
+++ Project/pygame_project.py
@@ -3,6 +3,7 @@ import random
 
 HANG_MAN_PICS = ['Sprites/1.jpg', 'Sprites/2.jpg', 'Sprites/3.jpg', 'Sprites/4.jpg', 'Sprites/5.jpg', 'Sprites/6.jpg',
                  'Sprites/7.jpg', 'Sprites/8.jpg', 'Sprites/9.jpg']
+
 missed_letters = ''
 correct_letters = ''
 text1 = ' '
@@ -10,16 +11,17 @@ GAME_IS_ON = True
 screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 W = pg.display.Info().current_w
 H = pg.display.Info().current_h
-# W = 1920
-# H = 1080
+
 MIN = min(H, W) - 200
 MAX = max(H, W)
-# screen = pg.display.set_mode((W, H))
+
 # устанавливаем размер шрифта
+
 FONT_SIZE = MIN // 16
+
 # вводим значения для координат текста с правильностью ввода
+
 coordinates_text_letter = (10, MIN + FONT_SIZE, MIN, FONT_SIZE)
-print(W, H)
 
 
 def get_random_word(word_list):
@@ -32,56 +34,70 @@ secret_word = get_random_word(words)
 
 
 def display_board(font):
+
     # выводим на экран картинку
+
     global GAME_IS_ON
     blank = '*' * len(secret_word)
 
     if len(missed_letters) < 16:
         if len(missed_letters) == 0:
             picture = pg.image.load(HANG_MAN_PICS[len(missed_letters)])
+
         else:
             picture = pg.image.load(HANG_MAN_PICS[len(missed_letters) - len(missed_letters) // 2])
         picture_rect = picture.get_rect()
         picture = pg.transform.scale(picture, (MIN, MIN))
         screen.blit(picture, (0, 0))
+
     else:
         picture = pg.image.load(HANG_MAN_PICS[len(missed_letters) - len(missed_letters) // 2])
         screen.blit(picture, (0, 0))
         GAME_IS_ON = False
+
         for i in range(len(secret_word)):
             if secret_word[i] in 'ёйцукенгшщзхъфывапролджэячсмитьбю':
                 blank = blank[:i] + secret_word[i] + blank[i + 1:]
+
         # размещение "звездочек"
+
         blank_font = font.render(blank, True, (133, 100, 8))
         screen.blit(blank_font, (10, MIN))
-    # picture_rect = picture.get_rect()
-    # screen.blit(picture, (100, 200))
+
     # выводим на экран список неправильных букв
+
     text_missed_letters = font.render('Неправильные буквы:' + missed_letters, True, (133, 100, 8))
     screen.blit(text_missed_letters, (MIN, 10))
 
     for i in range(len(secret_word)):
         if secret_word[i] in correct_letters:
             blank = blank[:i] + secret_word[i] + blank[i + 1:]
+
     blank_font = font.render(blank, True, (133, 100, 8))
     screen.blit(blank_font, (10, MIN))
+
     if blank.find('*') == -1:
         GAME_IS_ON = False
 
 
 def check_letters(letter, font):
     global correct_letters, missed_letters, text1
+
     text_letter = font.render('', True, (133, 100, 8))
     screen.blit(text_letter, coordinates_text_letter)
+
     if len(letter) > 1:
         text_letter = font.render('Введите одну букву кириллицы', True, (133, 100, 8))
         screen.blit(text_letter, coordinates_text_letter)
+
     elif (letter in missed_letters or letter in correct_letters) and len(letter) != 0:
         text_letter = font.render('Эту букву вы уже вводили', True, (133, 100, 8))
         screen.blit(text_letter, coordinates_text_letter)
+
     elif letter not in 'ёйцукенгшщзхъфывапролджэячсмитьбю':
         text_letter = font.render('Введите букву кириллицы', True, (133, 100, 8))
         screen.blit(text_letter, coordinates_text_letter)
+
     else:
         if letter in secret_word:
             correct_letters = correct_letters + letter
@@ -89,6 +105,7 @@ def check_letters(letter, font):
         else:
             missed_letters = missed_letters + letter + ' '
             text1 = ''
+
         text_letter = font.render('Введите одну букву кириллицы', True, (133, 100, 8))
         screen.blit(text_letter, coordinates_text_letter)
 
@@ -108,19 +125,23 @@ def main():
 
     while not done:
         display_board(font)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
+
             if event.type == pg.MOUSEBUTTONDOWN:
                 if input_box.collidepoint(event.pos):
                     active = not active
                 else:
                     active = False
                 color = color_active if active else color_inactive
+
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     screen.fill((0, 0, 0))
                     return
+
                 if active and GAME_IS_ON:
                     if event.key == pg.K_RETURN:
                         text1 = text
@@ -130,6 +151,7 @@ def main():
                         text = text[:-1]
                     else:
                         text += event.unicode
+
         screen.fill((255, 255, 255))
         txt_surface = font.render(text, True, color)
         width = max(200, txt_surface.get_width() + 10)
